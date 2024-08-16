b50dc7961ca070563cf54fdce87c7e88982b3277
Ibrahim El-Refaee <ibrahimrefaeei@gmail.com>
2024-08-16 12:32:57 +0300

    11.6.b

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,15 +16,23 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+  Product.findByPk(prodId)
+    .then((product) => {
       res.render('shop/product-detail', {
-        product: product[0],
+        product: product,
         pageTitle: 'product-detail',
         path: `/products/${prodId}`
       });
+      console.log(product);
     })
     .catch((err) => { console.log(err); })
+  // Product.findAll({where: {id: prodId}}).then((product)=>{
+  //   res.render('shop/product-detail', {
+  //           product: product[0],
+  //           pageTitle: 'product-detail',
+  //           path: `/products/${prodId}`
+  //         })}
+  // ).catch((err)=>{console.log(err);})
 }
 
 exports.getIndex = (req, res, next) => {
