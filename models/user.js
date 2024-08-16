e19ce3e6d400eed7e7db56dbf8398d0d1d9ef42e
Ibrahim El-Refaee <ibrahimrefaeei@gmail.com>
2024-08-16 14:08:55 +0300

    11.7

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const {Sequalize, DataTypes} = require('sequelize');
+const sequelize = require('../util/database');
+
+const User = sequelize.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = User;
