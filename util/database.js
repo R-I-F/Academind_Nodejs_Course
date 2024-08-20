2fa0035c720d765af0a88cf66710da12fec583df
Ibrahim El-Refaee <ibrahimrefaeei@gmail.com>
2024-08-20 11:06:23 +0300

    2.a->g

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
+const mongoConnect = (cb) => {
+  MongoClient.connect('mongodb+srv://ibrahim:r9GQE1Isy4v3wXuC@cluster0.moqb1.mongodb.net/?retryWrites=true&w=majority&appName=cluster0')
+  .then((result) => {
+     cb(result); 
+  })
+  .catch((err) => {
+     console.log(err); 
+  });
+}
+module.exports = mongoConnect;
