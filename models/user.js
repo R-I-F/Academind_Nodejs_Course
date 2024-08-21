c07274a6e1e00b6b0bec3a2d54bcbb6a2b9f75d2
Ibrahim El-Refaee <ibrahimrefaeei@gmail.com>
2024-08-21 14:25:43 +0300

    fix: 12.5.d.3

--- models/user.js
+++ models/user.js
@@ -1,16 +1,31 @@
-const Sequelize = require('sequelize');
+const getDb = require('../util/database').getDb;
+const { ObjectId } = require('mongodb');
 
-const sequelize = require('../util/database');
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+class User {
+  constructor(username, email){
+    this.username = uername;
+    this.email = email;
+  };
+
+  save(){
+    const db = getDb();
+    return db.collection('users')
+    .insertOne(this)
+    .then((result)=>{console.log(result);})
+    .catch((err)=>{console.log(err);})
+  }
+
+  static findById(userId){
+    const db = getDb();
+    return db.collection('users')
+    .findOne({_id: new ObjectId(userId)})
+    .then((result)=>{
+      return result
+    })
+    .catch((err)=>{ console.log(err); })    
+  };
+
+}
 
 module.exports = User;
