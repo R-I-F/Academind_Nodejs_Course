a8b593ae417b47075d893c8e965aa6d764069ca6
Ibrahim El-Refaee <ibrahimrefaeei@gmail.com>
2024-09-23 11:11:20 +0300

    21.1->5

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,8 +21,6 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  console.log(image);
-
   if(!image){
     return res.status(422).render('admin/edit-product', {
       pageTitle: 'Add Product',
--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,8 @@ const path = require('path');
 const fs = require('fs');
 const PDFDocument = require('pdfkit');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
@@ -38,7 +40,11 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = req.query.page;
+
   Product.find()
+    .skip((page-1)*ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
     .then(products => {
       res.render('shop/index', {
         prods: products,
